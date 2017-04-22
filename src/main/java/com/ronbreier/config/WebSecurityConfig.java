75d00050000c61d2ecb4c8ad84234c55581981d6
Ron Breier <breier.ron@gmail.com>
2017-04-22 11:18:08 -0500

    Setting frame options and changed file name of inventory modals

--- src/main/java/com/ronbreier/config/WebSecurityConfig.java
+++ src/main/java/com/ronbreier/config/WebSecurityConfig.java
@@ -33,6 +33,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception{
         http
+                .headers()
+                .frameOptions().sameOrigin()
+            .and()
                 .authorizeRequests()
                 .antMatchers("/css/**", "/js/**","/fonts/**","/images/**", "/", "/register", "/verifyemail/**")
                 .permitAll()
